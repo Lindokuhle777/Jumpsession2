31f218c196134cd192eb0f4f83471fb2b7dd5b92
ThapeloSeletisha <thapeloseletisha01@gmail.com>
2022-04-09 15:54:18 +0200

    integrate json

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -13,35 +13,36 @@ export class HomeComponent implements OnInit {
     "venue":"Times Square, Menlyn, ZA", 
     "date": "15 December, 2022", 
     "Description": "Marshmello finally comes to mzanzi for the most epic festival since Ramaphosa opened up the country. He will be accompanied by the best that our country has to offer.", 
-    "image": "marshmello-square.jpg", 
+    "image": "../../../assets/marshmello-square.jpg", 
     "Time": "18:00 - 06:00", 
-    "Title": "Keep it Mello",
+    "Title": "KEEP IT MELLO",
     "Category": "Festival"
   },
     
   {
     "id": 2, 
     "venue":"Times Square, Menlyn, ZA", 
-    "date": "15 December, 2022", 
-    "Description": "Marshmello finally comes to mzanzi for the most epic festival since Ramaphosa opened up the country. He will be accompanied by the best that our country has to offer.", 
-    "image": "marshmello-square.jpg", 
+    "date": "23 December, 2022", 
+    "Description": "The king of moshpits. The rowdiest of rowdy. As part of the Astrowrold tour, Travis Scott will grace us at Time Square.", 
+    "image": "../../../assets/travis-square.jpg", 
     "Time": "18:00 - 06:00", 
-    "Title": "Keep it Mello",
+    "Title": "ASTROWORLD",
     "Category": "Festival"
     }, 
     
     
   {
     "id": 3, 
-    "venue":"Times Square, Menlyn, ZA", 
-    "date": "15 December, 2022", 
-    "Description": "Marshmello finally comes to mzanzi for the most epic festival since Ramaphosa opened up the country. He will be accompanied by the best that our country has to offer.", 
-    "image": "marshmello-square.jpg", 
+    "venue":"Botanical Gardens, Randburg, ZA", 
+    "date": "30 December, 2022", 
+    "Description": "An event for the family. If you are a sucker for jazz, live bands and outdoors, this is for you. Bring your own snacks or buy from one of the tuckshops on the site.", 
+    "image": "../../../assets/picnic-square.png", 
     "Time": "18:00 - 06:00", 
-    "Title": "Keep it Mello",
+    "Title": "JAZZ GARDENS",
     "Category": "Festival"
     }
   ]
+  
   
 
 
